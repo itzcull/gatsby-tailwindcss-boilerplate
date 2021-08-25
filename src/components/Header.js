1c2db70d8e2d271a58a84bcff5ba14f3a7b4f824
Will Cullen <itzcull@pm.me>
2021-08-25 11:16:53 +1000

    Configured Gatsby Default Starter for Minimal TailwindCSS Starter

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header className="w-full mb-5 bg-gray-800">
+    {siteTitle}
+  </header>
+)
+
+export default Header
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,8 +9,9 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./Header"
+import Footer from "./Footer"
+import "../tailwind.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,27 +25,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div className="w-screen h-screen">
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      {children}
+      <Footer />
+    </div>
   )
 }
 
